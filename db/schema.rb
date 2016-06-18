fbc43fd6f2d220862da15b286aa575594e5b9d5e
Beston Nounrasi <bcreativer@gmail.com>
2016-06-18 13:06:09 +1000

    Fix admin

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160616031658) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean  "admin"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
